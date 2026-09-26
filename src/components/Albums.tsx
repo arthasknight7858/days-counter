b1ed14c1ba415332ff5ccf5d91bab25bff70e8b5
Axel <axel@example.com>
2026-09-26 13:29:30 -0500

    add fotos

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -38,6 +38,9 @@ const albums: AlbumItem[] = [
       "juntos 7.png",
       "juntos 8.png",
       "juntos 9.png",
+      "juntos 10.png",
+      "juntos 11.png",
+      "juntos 12.png",
     ],
   },
   {
@@ -92,6 +95,8 @@ const albums: AlbumItem[] = [
       "sofi44.jpeg",
       "sofi45.jpeg",
       "sofi46.jpeg",
+      "sofi47.jpeg",
+      "sofi48.jpeg",
     ],
   },
   {
